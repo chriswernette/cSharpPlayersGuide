2c5d9ed61bc32bda6f75e19b23be6e6b2d74cc26
Chris <chriswer@umich.edu>
2025-08-25 22:04:34 -0400

    made it thru one more level at home

---
+++ Level11/theMagicCannon/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+for (int i = 1; i < 101; i++)
+{
+  int fire = i % 3;
+  int electric = i % 5;
+  if ((fire == 0) && (electric == 0))
+  {
+    Console.ForegroundColor = ConsoleColor.Blue;
+    Console.WriteLine($"{i}: Power Burst!");
+  }
+  else if (fire == 0)
+  {
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine($"{i}: Fire!");
+  }
+  else if (electric == 0)
+  {
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine($"{i}: Electric!");
+  }
+  else
+  {
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.WriteLine($"{i}: Normal");
+  }
+}
---
+++ Level11/thePrototype/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int storedNum;
+
+//runs at least once to ask for number, if out of range, keeps asking
+do
+{
+  Console.Write("User 1, enter a number between 0 and 100: ");
+  storedNum = Convert.ToInt32(Console.ReadLine());
+}
+while ((storedNum < 0) || (storedNum > 100));
+
+//clear screen once good number is picked, so no cheating!
+Console.Clear();
+
+do
+{
+  Console.WriteLine("User 2, guess the number. ");
+  Console.Write("What is your next guess? ");
+  int guessNum = Convert.ToInt32(Console.ReadLine());
+
+  if(guessNum == storedNum)
+  {
+    Console.WriteLine("You guessed the number!");
+    break;
+  } else if(guessNum < storedNum){
+    Console.WriteLine("You guessed too low!");
+  } else
+  {
+    Console.WriteLine("You guessed too high!");
+  }
+}
+while (true);
