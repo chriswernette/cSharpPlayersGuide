2054f3ac7176c65aaca505ad9f9e009748c641f5
Chris <chriswer@umich.edu>
2025-10-13 22:41:23 -0400

    realized I stayed on level 13 branch for too long by accident. Oops. Probably doesn't make sense to branch for these small projects unless iterating on them

---
+++ Level24/ThePoint/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics.Contracts;
+using System.Runtime.CompilerServices;
+
+Console.WriteLine("Hello, World!");
+
+Point myPoint1 = new Point(2, 3);
+Point myPoint2 = new Point(-4, 0);
+
+Console.WriteLine($"( {myPoint1.X}, {myPoint1.Y} )");
+Console.WriteLine($"( {myPoint2.X}, {myPoint2.Y} )");
+
+
+public class Point
+{
+  public int X { get; private set; } = 0;
+  public int Y { get; private set; } = 0;
+
+  public Point() : this(0, 0) { }
+  public Point(int x, int y)
+  {
+    this.X = x;
+    this.Y = y;
+  }
+}
+
