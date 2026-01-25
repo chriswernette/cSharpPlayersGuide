e4b0487d29adb8ab3088be77c87da0cee38e944c
Chris <chriswer@umich.edu>
2026-01-25 09:45:49 -0500

    making good progress on more Obj Oriented version of the puzzle, time to commit. Have the player, map, and game classes done. Need to work on senses.

--- Level31/TheFountainOfObjects/Program.cs
+++ Level31/TheFountainOfObjects/Program.cs
@@ -40,371 +40,184 @@
  * Once tested the redesigned game, add 2 more expansion packs! Maelstrom and arrow?
  */
 
-Console.WriteLine("-----------------------------------------------");
-Console.Write("What size game would you like to play? Small, medium, or large: ");
-string? gameSize = null;
-
-while(gameSize == null)
-{
-  gameSize = Console.ReadLine();
-  if(gameSize == null)
-  {
-    Console.WriteLine("Enter something dummy!");
-  }
-}
-
-Game myGame = new Game(gameSize);
-
-Console.WriteLine("-----------------------------------------------");
-
-bool winner;
-bool dead;
-while(true)
-{
-  myGame.PlayGame();
-  winner = myGame.CheckForWin();
-  dead = myGame.CheckInPit(false);
-  if (winner)
-  {
-    break;
-  }
-  if (dead)
-  {
-    break;
-  }
-}
-//final announcement, whick we wouldn't normally reach
-myGame.AnnounceRoomLocationEffects();
 
 
-public class Game
-{
-  public bool FountainEnabled = false;
-  public int[] PlayerLocation = { 0, 0 };
-  private static int[] FountainLocation = { 0, 2 };
-  public static readonly int[] EntranceLocation = { 0, 0 };
-  public static int[][] PitLocations = new int[3][]; 
-  private int GameSize = 3;
-  public static readonly int SmallGameSize = 3;
-  public static readonly int MediumGameSize = 5;
-  public static readonly int LargeGameSize = 7;
-
-  public Game(string StringGameSize)
-  {
-    StringGameSize = StringGameSize.Trim().ToLower();
-    switch (StringGameSize)
-    {
-      case "small":
-        GameSize = 3;
-        FountainLocation = new int[] { 0, 2 };
-        PitLocations[0] = new int[] { 0, 3 };
-        PitLocations[1] = new int[] { 10, 10 };
-        PitLocations[2] = new int[] { 10, 10 };
-        break;
-      case "medium":
-        GameSize = 5;
-        FountainLocation = new int[] { 2, 2 };
-        PitLocations[0] = new int[] { 0, 3 };
-        PitLocations[1] = new int[] { 3, 4 };
-        PitLocations[2] = new int[] { 10, 10 };
-        break;
-      case "large":
-        GameSize = 7;
-        FountainLocation = new int[] { 2, 4 };
-        PitLocations[0] = new int[] { 0, 3 };
-        PitLocations[1] = new int[] { 3, 4 };
-        PitLocations[2] = new int[] { 4, 6 };
-        break;
-      default:
-        GameSize = 3;
-        FountainLocation = new int[] { 0, 2 };
-        break;
-    }
-  }
-
-  public Game()
-  {
-    GameSize = 3;
-    FountainLocation = new int[] { 0, 2 };
-    PitLocations[0] = new int[] { 0, 3 };
-    PitLocations[1] = new int[] { 10, 10 };
-    PitLocations[2] = new int[] { 10, 10 };
-  }
-
-  public void PlayGame()
-  {
-    /*Announce the room location effects which could be fountain, entrance, or win, or more in the future
-    * This needs to be called first, and then take the user input..
-    */
-    AnnounceRoomLocationEffects();
-
-    string? turnCommand = TakeUserInput();
-    if(turnCommand != null)
-    {
-      //make standard input
-      string formattedInput = turnCommand.Trim().ToLower();
-
-      //determine if they wanted to move or enable the fountain, if yes, then enable if in the correct room
-      bool enable = EnableOrMove(formattedInput);
-      if (enable)
-      {
-        bool inRoom = CheckInFountainRoom(false);
-        EnableFountain(inRoom);
-      }
-      else
-      {
-        //convert string to coordinate notation, then try to move (might hit a wall)
-        int[] moveCommand = StringToCoord(formattedInput);
-        UpdatePlayerLocation(moveCommand);
-      } 
-    }
-    else //null check
-    {
-      Console.ForegroundColor = ConsoleColor.Magenta;
-      Console.WriteLine("Enter something next time dummy!");
-      Console.ForegroundColor = ConsoleColor.White;
-    }
-}
-
-  /* AnnounceRoomLocationEffects
-   * This function will print the player's current location as well as any effects of the room they are in or nearby rooms
-   */
-  public void AnnounceRoomLocationEffects()
-  {
-    Console.ForegroundColor = ConsoleColor.Magenta;
-    Console.WriteLine($"You are in the room at (Row={PlayerLocation[0]}, Column={PlayerLocation[1]})");
-    Console.ForegroundColor = ConsoleColor.White;
-
-    CheckInFountainRoom(true); //we will add more checks later in the game and call them one after another
-    CheckForPitNearby();
-    CheckInPit(true);
-    CheckForEntrance();
-  }
-
-  /* TakeUserInput
-   * This function will announce where the user is, print any statuses of that room or adjacent rooms, 
-   * and take user input from the command line and trim and lowercase it, for consumption in other functions like 
-   * StringToCoord. However, it will not do error checking if it's a valid input, which will be done in those functions.
-   */
-  public string? TakeUserInput()
-  {
-    string? command;
-    Console.ForegroundColor = ConsoleColor.Magenta;
-    Console.Write("What do you want to do? ");
-    Console.ForegroundColor = ConsoleColor.Cyan;
-    command = Console.ReadLine();
-    Console.ForegroundColor = ConsoleColor.White;
-    Console.WriteLine("-------------------------------------------------------------");
-    return command;
-  }
-  
-  /* StringToCoord
-   * Converts text player has entered on the command line to a move command
-   */
-  public int[] StringToCoord(string input)
-  {
-    int[] intCoord = { 0, 0 };
-    if (input == null)
-    {
-      Console.ForegroundColor = ConsoleColor.Magenta;
-      Console.WriteLine("Input something next time!");
-      Console.ForegroundColor = ConsoleColor.White;
-    }
-    else if (input == "move north")
-    {
-      intCoord[0] = -1;
-    }
-    else if (input == "move south")
-    {
-      intCoord[0] = 1;
-    }
-    else if (input == "move east")
-    {
-      intCoord[1] = 1;
-    }
-    else if (input == "move west")
-    {
-      intCoord[1] = -1;
-    }
-    else if (input == "enable fountain")
-    {
-    }
-    else
-    {
-      Console.ForegroundColor = ConsoleColor.Magenta;
-      Console.WriteLine("Make a valid command next time");
-      Console.ForegroundColor = ConsoleColor.White;
-    }
-    return intCoord;
-  }
-
-  /* UpdatePlayerLocation
-   * Takes the move in coordinate pair as integer and updates the players location on the map.
-   * Takes into account the boundaries of the map and clips the player's location.
-   */
-  public void UpdatePlayerLocation(int[] playerMove)
-  {
-    for (int i = 0; i < playerMove.Length; i++)
-    {
-      PlayerLocation[i] += playerMove[i];
-      //check for out of range and display a message if the player broke the rules
-      if (PlayerLocation[i] < 0)
-      {
-        Console.ForegroundColor = ConsoleColor.Magenta;
-        Console.WriteLine("Error, you walked off the edge of the map! Resetting you to your last location");
-        Console.ForegroundColor = ConsoleColor.White;
-        PlayerLocation[i] = 0;
-      }
-      if (PlayerLocation[i] > GameSize)
-      {
-        Console.ForegroundColor = ConsoleColor.Magenta;
-        Console.WriteLine("Error, you walked off the edge of the map! Resetting you to your last location");
-        Console.ForegroundColor = ConsoleColor.White;
-        PlayerLocation[i] = GameSize;
-      }
-    }
-  }
-
-  /*EnableOrMove
-   * Determines if the player wants to try to turn on the fountain, or to move. Happens no matter what
-   */
-  public bool EnableOrMove(string input)
-  {
-    bool enable = false; 
-    if (input == "enable fountain")
-    {
-      enable = true;
-    }
-    return enable;
-  }
-
-  /*EnableFountain
-   * Turns on the fountain of objects!
-   */
-  public void EnableFountain(bool inRoom)
-  {
-    if (inRoom)
-    {
-      FountainEnabled = true;
-    }
-    else
-    {
-      Console.ForegroundColor = ConsoleColor.Magenta;
-      Console.WriteLine("You are not in the fountain room, you dufus.");
-      Console.ForegroundColor = ConsoleColor.White;
-    }
-  }
-
- /*CheckInFountainRoom
- * Returns true if you are in the fountain room. Announces the fountain text if called on
+/*CLASS LIST
+ * Game - contains map (small, med, large), monsters, player, fountain enabled
+ * Map - nxn set of rooms
+ * Room - contains roomtype, monster or no monster, fountain or no fountain? 
+ * Console helper - can write message to console or print those break line things?
+ * 
+ * 
  */
-  public bool CheckInFountainRoom(bool Announce)
+
+/*METHOD LIST
+ * Console helper - prints things to the console in pretty formats/colors, also takes user input and formats it or re-prompts?
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+public class Player
+{
+  public Location playerLocation = new Location(0,0);
+  public bool isAlive;
+  public string deathCause;
+  public int arrowsRemaining;
+
+  //initializer
+  public Player(int row, int col)
   {
-    bool inFountainRoom = false;
-    if ((PlayerLocation[0] == FountainLocation[0]) && (PlayerLocation[1] == FountainLocation[1]))
-    {
-      inFountainRoom = true;
-      if (Announce)
-      {
-        if (FountainEnabled)
-        {
-          Console.ForegroundColor = ConsoleColor.Blue;
-          Console.WriteLine("You hear the rushing waters from the Fountain of Objects. It has been reactivated!");
-          Console.ForegroundColor = ConsoleColor.White;
-        }
-        else
-        {
-          Console.ForegroundColor = ConsoleColor.Blue;
-          Console.WriteLine("You hear water dripping in this room. The Fountain of Objects is here!");
-          Console.ForegroundColor = ConsoleColor.White;
-        }
-      }
-    }
-    return inFountainRoom;
+    playerLocation = new Location(row, col);
+    isAlive = true;
+    deathCause = string.Empty;
+    arrowsRemaining = 5;
   }
-
-  /*CheckForPitNearby
-   * At the end of every turn, check if the player is 1 room away from a pit and make the announcement if so
-   * I.e. if they are due west of pit room, or if they are north, or northwest, etc. 1 away left or right and 1 away up or down
-   * Will have to use abs function I think... Check all 3 pit locations every time, since the 2 other pits will be placed
-   * off the map if it's a small room, it shouldn't detect them..
-   */
-
-
-  //TODO - debug, I think the coords are messed up maybe? It's saying you're near a pit when you're not actually
-  public void CheckForPitNearby()
+  //returns list of senses and prints out messages for each
+  public List<Sense> SenseNearbyRooms(Map map)
   {
-    for (int i = 0; i < PitLocations.Length; i++)
-    {
-      int xDistance = Math.Abs(PitLocations[i][0] - PlayerLocation[0]);
-      int yDistance = Math.Abs(PitLocations[i][1] - PlayerLocation[1]);
-      //want to announce if close to pit room, but not if in the pit room, because then we will announce death instead.
-      if((xDistance <= 1) && (yDistance <= 1) && ((xDistance + yDistance) != 0))
-      {
-        Console.WriteLine("You feel a draft. There is a pit nearby.");
-      }
-    }
-  }
+    ConsoleHelper helper = new ConsoleHelper();
+    List<Sense> senses = new List<Sense>();
 
-  /*CheckInPit
-   * At the end of every turn, if the player stepped into the pit, they die, and the game ends.
-   */
-  public bool CheckInPit(bool announce)
-  {
-    bool died = false;
-    for (int i = 0; i < PitLocations.Length; i++)
+    for (int i = playerLocation.X - 1; i > playerLocation.X; i++)
     {
-      bool xTrue = PitLocations[i][0] == PlayerLocation[0];
-      bool yTrue = PitLocations[i][1] == PlayerLocation[1];
-      if (xTrue && yTrue)
+      for (int j = playerLocation.Y - 1; j > playerLocation.Y; j++)
       {
-        died = true;
-        if (announce)
+        if (map.Rooms[i, j] == RoomType.Pit)
         {
-          Console.ForegroundColor = ConsoleColor.Red;
-          Console.WriteLine("You fell into a pit! You have died. Game over.");
-          Console.ForegroundColor = ConsoleColor.White;
+          senses.Add(Sense.Pit);
+          helper.printLine("You feel a draft. There is a pit in a nearby room.", ConsoleColor.White); // what if multiple pits? This will print multiple times? OK?
         }
       }
     }
-    return died;
+    if (map.Rooms[playerLocation.X, playerLocation.Y] == RoomType.Entrance)
+    {
+      senses.Add(Sense.Light);
+      helper.printLine("You see light coming from the cavern entrance.", ConsoleColor.Yellow);
+    }
+    if (map.Rooms[playerLocation.X, playerLocation.Y] == RoomType.Fountain)
+    {
+      senses.Add(Sense.Fountain);
+      if(map.CheckFountainEnabled() == false)
+        helper.printLine("You hear water dripping in this room. The fountain of objects is here!", ConsoleColor.Blue);
+      if (map.CheckFountainEnabled() == true)
+        helper.printLine("You hear the rushing waters from the fountain of objects. It has been reactivated!", ConsoleColor.Blue); //Should we also print something like this when they reactivate?
+    }
+    return senses;
   }
 
-  /*CheckForEntrance
-   * At the end of every step, checks to see if the player is at the entrance and 
-   * if they have won the game and display a congrats message if so
-   */
-  public void CheckForEntrance()
+
+  public void GetKilled(string reasonYouDied)
   {
-    if ((PlayerLocation[0] == EntranceLocation[0]) && (PlayerLocation[1] == EntranceLocation[1]))
-    {
-      if (FountainEnabled == true)
-      {
-        Console.ForegroundColor = ConsoleColor.Green;
-        Console.WriteLine("The Fountain of Objects has been reactivated, and you have escaped with your life! You win!");
-        Console.ForegroundColor = ConsoleColor.White;
-      }
-      else
-      {
-        Console.ForegroundColor = ConsoleColor.Yellow;
-        Console.WriteLine("You see light in this room coming from outside the cavern. This is the entrance.");
-        Console.ForegroundColor = ConsoleColor.White;
-      }
-    }
+    this.isAlive = false;
+    this.deathCause = reasonYouDied;
   }
 
-  /*CheckForWin
-   * used to escape the forever while loop in the main program
-   */
-  public bool CheckForWin()
+  public void MoveCommand(Direction dir, int mapSize)
   {
-    if ((PlayerLocation[0] == EntranceLocation[0]) && (PlayerLocation[1] == EntranceLocation[1]))
+    if (dir == Direction.North)
+      playerLocation.Y = ClampAndReport(playerLocation.Y - 1, 0, mapSize);
+    if (dir == Direction.East)
+      playerLocation.X = ClampAndReport(playerLocation.X + 1, 0, mapSize);
+    if (dir == Direction.South)
+      playerLocation.Y = ClampAndReport(playerLocation.Y + 1, 0, mapSize);
+    if(dir == Direction.West)
+      playerLocation.X = ClampAndReport(playerLocation.X - 1, 0, mapSize);
+  }
+  private int ClampAndReport(int value, int min, int max)
+  {
+    if(value < min || value > max)
     {
-      if (FountainEnabled == true)
-      {
-        return true;
-      }
+      ConsoleHelper helper = new ConsoleHelper();
+      helper.printLine("You tried to walk off the edge of the map! Resetting your position.", ConsoleColor.Red);
+      helper.blockLine();
     }
-    return false;
+    return Math.Clamp(value, min, max);
+  }
+
+  public bool checkForDeath()
+  {
+    return this.isAlive;
+  }
+
+  public void FireArrow(Direction dir)
+  {
+    this.arrowsRemaining--;
+    //TODO add direction and monsters
+  }
+
+
+
+}
+
+public class Map 
+{
+  int Row {  get; }
+  int Column { get; }
+  bool FountainEnabled { get; set; }
+  public RoomType [,] Rooms;
+  public Map(int row, int column)
+  {
+    Rooms = new RoomType[row, column];
+    Row = row;
+    Column = column;
+    FountainEnabled = false;
+  }
+  public bool CheckFountainEnabled()
+  {
+    return FountainEnabled;
+  }
+  public void EnableFountain()
+  {
+    FountainEnabled = true;
   }
 }
+
+public class ConsoleHelper
+{
+  public void printLine(string line, ConsoleColor color)
+  {
+    Console.ForegroundColor = color;
+    Console.WriteLine(line);
+  }
+  public void blockLine()
+  {
+    Console.WriteLine("--------------------------------------------------------------");
+  }
+
+  public void startGameText()
+  {
+    Console.WriteLine("You enter the Cavern of Objects, a maze of rooms filled with dangerous pits in search of the Fountain of Objects.");
+    Console.WriteLine("Light is visible only in the entrance, and no other light is seen anywhere in the caverns.");
+    Console.WriteLine("You must navigate the Caverns with your other senses.");
+    Console.WriteLine("Find the Fountain of Objects, activate it, and return to the entrance.");
+    Console.WriteLine("Look out for pits. You will feel a breeze if a pit is in an adjacent room.");
+    Console.WriteLine("Maelstroms are violent forces of sentient wind. Entering a room with one could transport you to any other location in the caverns." +
+      "You will be able to hear their growling and groaning in nearby rooms.");
+    Console.WriteLine("Amoraks roam the caverns. Encountering one is certain death, but you can smell their rotten stench in nearby rooms.");
+    Console.WriteLine("You carry with you a bow and a quiver of arrows. You can use them to shoot monsters in the caverns but be warned you have a limited supply.");
+  }
+  public void helpText()
+  {
+    Console.WriteLine("There are four main forms of commands: typing 'help' displays this message");
+    Console.WriteLine("Typing 'enable fountain' attempts to enable the fountain of objects, but will only work if you are in the fountain room");
+    Console.WriteLine("Typing 'move' + a direction will attempt to move you in that direction, for example 'move east'. But you cannot walk off the map.");
+    Console.WriteLine("Typing 'shoot' + a direction will attempt to fire an arrow in that direction. Be warned that you have a limited number of arrows!");
+    Console.WriteLine("Typing anything else will have no affect.");
+  }
+}
+
+public struct Location
+{
+  public int X { get; set; }
+  public int Y { get; set; }
+  public Location(int x, int y)
+  {
+    X = x;
+    Y = y;
+  }
+};
+public enum Direction { North, East, South, West}
+public enum Sense { NoSense, Light, Fountain, Pit}
+public enum RoomType { Empty, Entrance, Fountain, Pit}
