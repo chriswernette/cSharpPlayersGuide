6f4a015330b74f58a850223b2c8d4f9b46031b05
Chris <chriswer@umich.edu>
2026-02-27 05:45:04 -0500

    realized that level 48 was somehow stored in the wrong directory and didn't make it to github. Probably when I installed VS 2026 it changed to C:/Users/Me/source/repos by default. I much prefer storing on D:/ and with shorter path

---
+++ Level48/ColoredConsole.Common/ColoredConsole.cs
@@ -0,0 +1,23 @@
+﻿namespace Helpers
+{
+  public static class ColoredConsole
+  {
+    public static string? Prompt(string question)
+    {
+      string output;
+      Console.ForegroundColor = ConsoleColor.Cyan;
+      Console.WriteLine(question);
+      return Console.ReadLine();
+    }
+    public static void WriteLine(string? text, ConsoleColor color)
+    {
+      Console.ForegroundColor = color;
+      Console.WriteLine(text);
+    }
+    public static void Write(string? text, ConsoleColor color)
+    {
+      Console.ForegroundColor = color;
+      Console.Write(text);
+    }
+  }
+}
---
+++ Level48/ColoredConsole/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Helpers;
+
+string? name = ColoredConsole.Prompt("What is your name?");
+ColoredConsole.WriteLine("Hello " + name, ConsoleColor.Green);
